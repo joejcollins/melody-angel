5311505c7f58b012afd70f2bb846ba913cc716b6
Joe Collins <j.collins@zengenti.com>
2023-02-16 15:25:18 +0000

    Have a go heros

--- src/resistor_color.rs
+++ src/resistor_color.rs
@@ -1,23 +1,37 @@
 #[derive(Debug, PartialEq, Eq)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
 pub fn color_to_value(color: ResistorColor) -> u32 {
-    unimplemented!("convert color {color:?} into a numerical representation")
+    match color {
+        ResistorColor::Black => 0,
+        ResistorColor::Brown => 1,
+        ResistorColor::Red => 2,
+        ResistorColor::Orange => 3,
+        ResistorColor::Yellow => 4,
+        ResistorColor::Green => 5,
+        ResistorColor::Blue => 6,
+        ResistorColor::Violet => 7,
+        ResistorColor::Grey => 8,
+        ResistorColor::White => 9,
+    }
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-    unimplemented!("convert the value {value} into a string representation of color")
+    match value {
+        all::<ResistorColor>().
+    }
+    "Red".to_string()
 }
 
 pub fn colors() -> Vec<ResistorColor> {
--- tests/resistor_color.rs
+++ tests/resistor_color.rs
@@ -1,43 +1,36 @@
 use melody_angel::resistor_color::{color_to_value, colors, value_to_color_string, ResistorColor};
 
 #[test]
-#[ignore]
 fn test_black() {
     assert_eq!(color_to_value(ResistorColor::Black), 0);
 }
 
 #[test]
-#[ignore]
 fn test_orange() {
     assert_eq!(color_to_value(ResistorColor::Orange), 3);
 }
 
 #[test]
-#[ignore]
 fn test_white() {
     assert_eq!(color_to_value(ResistorColor::White), 9);
 }
 
 #[test]
-#[ignore]
 fn test_2() {
     assert_eq!(value_to_color_string(2), String::from("Red"));
 }
 
 #[test]
-#[ignore]
 fn test_6() {
     assert_eq!(value_to_color_string(6), String::from("Blue"));
 }
 
 #[test]
-#[ignore]
 fn test_8() {
     assert_eq!(value_to_color_string(8), String::from("Grey"));
 }
 
 #[test]
-#[ignore]
 fn test_11_out_of_range() {
     assert_eq!(
         value_to_color_string(11),
@@ -46,7 +39,6 @@ fn test_11_out_of_range() {
 }
 
 #[test]
-#[ignore]
 fn test_all_colors() {
     use ResistorColor::*;
     assert_eq!(
