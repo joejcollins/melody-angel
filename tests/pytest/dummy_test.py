64ecdb7bc6ba40740fcfe9c3a8663a55b67f385a
Joe Collins <joejcollins@gmail.com>
2023-06-25 19:29:07 +0000

    Dummy tests

---
+++ tests/pytest/dummy_test.py
@@ -0,0 +1,9 @@
+def test_nothing() -> None:
+    """Test that the static settings are correct."""
+    # Arrange
+    number: int = 0
+    # Act
+    number = 1 + 2
+    number -= 2
+    # ASSERT
+    assert number == 1
