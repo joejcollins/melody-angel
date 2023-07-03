a697b43f9a97a5fa8b0dcac0de0dd611e311324a
Joe Collins <joejcollins@gmail.com>
2023-07-03 15:53:48 +0000

    Demo dataset

--- python_src/handlers/hill_heights.py
+++ python_src/handlers/hill_heights.py
@@ -1,5 +1,6 @@
 """ Transform GPX into csv. """
 import csv
+from python_src.models.hill_height_point import SamplePoint
 
 import gpxpy
 
@@ -8,22 +9,41 @@ CSV_FILE = "data/processed/hill_heights.csv"
 
 
 def make_hill_heights() -> None:
-    """ Use an xslt to add put the id in the gpx name element """
+    """Use an xslt to add put the id in the gpx name element"""
     # Parse GPX file
-    with open(GPX_FILE, 'r') as f:
+    with open(GPX_FILE, "r") as f:
         gpx = gpxpy.parse(f)
 
     # Extract waypoints
-    waypoints = []
+    sample_waypoints = []
     for waypoint in gpx.waypoints:
-        name = waypoint.name or ''
-        latitude = waypoint.latitude or ''
-        longitude = waypoint.longitude or ''
-        elevation = waypoint.elevation or ''
-        waypoints.append([name, latitude, longitude, elevation])
+        name = waypoint.name or ""
+        latitude = waypoint.latitude or ""
+        longitude = waypoint.longitude or ""
+        elevation = waypoint.elevation or ""
+        sample_waypoints.append(
+            {
+                "id": name,
+                "latitude": latitude,
+                "longitude": longitude,
+                "elevation": elevation,
+            }
+        )
 
     # Write waypoints to CSV file
-    with open(CSV_FILE, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['name', 'latitude', 'longitude', 'elevation'])
-        writer.writerows(waypoints)
+    with open(CSV_FILE, "w+", newline="") as records_file:
+        record_writer = csv.DictWriter(
+            records_file,
+            fieldnames=[
+                "id",
+                "latitude",
+                "longitude",
+                "elevation",
+                "eastings",
+                "northings",
+            ],
+        )
+        record_writer.writeheader()
+        for sample_waypoint in sample_waypoints:
+            sample_point = SamplePoint(**sample_waypoint)
+            record_writer.writerow(sample_point.dict())
--- python_src/models/hill_height_point.py
+++ python_src/models/hill_height_point.py
@@ -1,13 +1,26 @@
 """Schema for a sampling point."""
 from pydantic import BaseModel
+from OSGridConverter import latlong2grid
 
 
 class SamplePoint(BaseModel):
     """A sample point with both OS grid and the Geographic Coordinate System."""
-    id: int
-    description: str
-    eastings: int
-    northings: int
+
+    id: str
     latitude: float
     longitude: float
-    os_grid_ref: str
+    elevation: float
+
+    @property
+    def eastings(self):
+        return latlong2grid(self.latitude, self.longitude, tag="WGS84").E
+
+    @property
+    def northings(self):
+        return latlong2grid(self.latitude, self.longitude, tag="WGS84").N
+
+    def dict(self, *args, **kwargs):
+        result = super().dict(*args, **kwargs)
+        result['eastings'] = self.eastings
+        result['northings'] = self.northings
+        return result
