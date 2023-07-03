e2f4377bc2f00745e3eb7e5faf528ee797bbab3c
Joe Collins <joejcollins@gmail.com>
2023-07-03 14:00:24 +0000

    make demo dataset

--- python_src/handlers/hill_heights.py
+++ python_src/handlers/hill_heights.py
@@ -1,34 +1,29 @@
-""" Transform GPX
+""" Transform GPX into csv. """
+import csv
 
-View Ranger doesn't read the gpx extensions so in order
-to identify sample sites the id element is transformed into
-a gpx field. """
-import os
-import lxml.etree as ET
+import gpxpy
 
-FILES = ["trial-waypoints.gpx", "spains-hall-waypoints-regular-30m.gpx"]
+GPX_FILE = "data/raw/Waypoints_03-JUL-23.gpx"
+CSV_FILE = "data/processed/hill_heights.csv"
 
 
-def main() -> None:
-    """ Transform all the files """
-    for gpx_file in FILES:
-        transform(gpx_file)
-
-
-def transform(gpx_file: str) -> None:
+def make_hill_heights() -> None:
     """ Use an xslt to add put the id in the gpx name element """
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    gpx_file_name = os.path.join(dir_path, f"../data/raw/{gpx_file}")
-    dom = ET.parse(gpx_file_name)
-    xslt_file_name = os.path.join(dir_path, "add_id_to_waypoints.xslt")
-    xslt = ET.parse(xslt_file_name)
-    xslt_transform = ET.XSLT(xslt)
-    new_dom = xslt_transform(dom)
-    file_transformed = ET.tostring(new_dom, pretty_print=True)
-    output_file_name = gpx_file_name.replace(".gpx", "-with-name.gpx")
-    output_file = open(output_file_name, 'wb')
-    output_file.write(file_transformed)
+    # Parse GPX file
+    with open(GPX_FILE, 'r') as f:
+        gpx = gpxpy.parse(f)
 
+    # Extract waypoints
+    waypoints = []
+    for waypoint in gpx.waypoints:
+        name = waypoint.name or ''
+        latitude = waypoint.latitude or ''
+        longitude = waypoint.longitude or ''
+        elevation = waypoint.elevation or ''
+        waypoints.append([name, latitude, longitude, elevation])
 
-if __name__ == "__main__":
-    main()
+    # Write waypoints to CSV file
+    with open(CSV_FILE, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['name', 'latitude', 'longitude', 'elevation'])
+        writer.writerows(waypoints)
--- python_src/make_datasets.py
+++ python_src/make_datasets.py
@@ -1,8 +1,11 @@
 """Create the datasets."""
+from python_src.handlers.hill_heights import make_hill_heights
+
 
 def create_hill_heights_table() -> None:
     """Create the hill_heights table."""
-    pass
+    make_hill_heights()
+
 
 if __name__ == "__main__":
     create_hill_heights_table()
