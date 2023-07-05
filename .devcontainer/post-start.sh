710efce942d60e8349f2b8d056084968253bcef2
Joe J Collins <joejcollins@users.noreply.github.com>
2023-07-05 16:38:28 +0000

    Mounts and user permissions

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,7 +1,7 @@
 sudo bash -c "echo R_LIBS_USER=/workspaces/melody-angel/.R/library > /home/rstudio/.Renviron"
-# ln -s /workspace/melody-angel /home/codespace/melody-angel
+ln -s /workspace/melody-angel /home/rstudio/melody-angel
 # https://stackoverflow.com/questions/47541007/how-to-i-bypass-the-login-page-on-rstudio
-# sudo usermod -a -G sudo codespace
+sudo usermod -a -G sudo rstudio
 sudo bash -c "echo 'server-user=rstudio' >> /etc/rstudio/rserver.conf"
 sudo bash -c "echo 'auth-none=1' >> /etc/rstudio/rserver.conf"
 # Restart the rserver with sudo otherwise it won't run for the rstudio user (dunno why)
