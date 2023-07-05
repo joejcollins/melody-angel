05bca31cdb4c90b73f8ed5f0cae32bbbcd4f3de8
Joe J Collins <joejcollins@users.noreply.github.com>
2023-07-05 15:32:07 +0000

    rstudio config

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,10 +1,10 @@
-sudo bash -c "echo R_LIBS_USER=/workspaces/melody-angel/.R/library > /home/codespace/.Renviron"
-ln -s /workspace/melody-angel /home/codespace/melody-angel
+sudo bash -c "echo R_LIBS_USER=/workspaces/melody-angel/.R/library > /home/rstudio/.Renviron"
+# ln -s /workspace/melody-angel /home/codespace/melody-angel
 # https://stackoverflow.com/questions/47541007/how-to-i-bypass-the-login-page-on-rstudio
-sudo usermod -a -G sudo codespace
-sudo bash -c "echo 'server-user=codespace' >> /etc/rstudio/rserver.conf"
+# sudo usermod -a -G sudo codespace
+sudo bash -c "echo 'server-user=rstudio' >> /etc/rstudio/rserver.conf"
 sudo bash -c "echo 'auth-none=1' >> /etc/rstudio/rserver.conf"
-# Restart the rserver with sudo otherwise it won't run for the codespace user (dunno why)
+# Restart the rserver with sudo otherwise it won't run for the rstudio user (dunno why)
 sudo rserver
 sudo pkill rserver
 alias python='python3'
