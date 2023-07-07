93d5ebbf17a7dd746bd206d8f5b419b6dc707219
jaredf j <jared0261@hotmail.co.uk>
2023-07-07 09:19:47 +0000

    update plymouth

--- R/plymouth_demo.R
+++ R/plymouth_demo.R
@@ -4,6 +4,7 @@ library(readr)
 library(ggplot2)
 library(tibble)
 library(dplyr)
+library(tidyverse)
 
 plymouth <- read_csv("~/melody-angel/data/raw/plymouth.csv")
 
@@ -22,20 +23,24 @@ crap <- crap[-1,]
 
 crap <- tibble::rownames_to_column(crap, "Age")
 
-class(crap$Central) = "integer"
-class(crap$Crownhill) = "integer"
-class(crap$Devonport) = "integer"
-class(crap$Efford) = "integer"
-class(crap$Estover) = "integer"
-class(crap$`North Prospect`) = "integer"
-class(crap$Peverell) = "integer"
-class(crap$Plympton) = "integer"
-class(crap$Plymstock) = "integer"
-class(crap$Southway) = "integer"
-class(crap$`St Budeaux`) = "integer"
-class(crap$`West Park`) = "integer"
+for (i in colnames(crap)){
+  if(i != "Age"){
+    crap[, i] <- as.numeric(crap[, i])
+  }
+}
 
-ggplot(data=crap, aes(x=factor(Age, Age), y=Crownhill, group=1)) +
+ggplot(data = crap, aes(x= Age, y = Crownhill, group = 1)) +
   geom_line() +
   geom_point()
+
+# Reshape the data into a longer format
+crap_long <- crap %>%
+  pivot_longer(cols = -Age, names_to = "Location", values_to = "Value")
+
+# Plot the line plot
+ggplot(data = crap_long, aes(x = Age, y = Value, group = Location, color = Location)) +
+  geom_line() +
+  geom_point()
+
+  
 
